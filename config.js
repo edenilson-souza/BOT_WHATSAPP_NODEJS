bd19460edb764d1377d4f23151294235a87eed59
Edenilson Souza <edenilson.sza@gmail.com>
2022-09-24 20:59:56 -0300

    a

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // API reference URL
+    apiUrl: process.env.API_URL || "",
+    // API token from your personal cabinet
+    token: process.env.TOKEN || "EAAFuiawAhM4BAOHQ7yCZC20cFLRByu6yvMKhlZAed1l8QqM8NrD9MXg8qQCgItgpZCQ2jBLxpM6J6MHXSAwlZCg12VjP69dH2mX71R9LZBu6L2oYmLe475fBZBe7sSTnn5v9XzfZAIRVYfe8ppZBRp5pHWbNuZAKSPqfZBXJiMHf4DYiKRYSOEHTwmDCc4cY2yB0GK9kufUaWTjosFiZBrscNIlug4NYKkQi2oZD",
+    // Bot`s URL (for static file)
+    botUrl: process.env.BOT_URL || ""
+
+}
