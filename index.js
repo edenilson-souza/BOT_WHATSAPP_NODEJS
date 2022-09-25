427586fb8e31e1248ebfced300e2b0472cf9811c
Fallitz <edenilson.sza@gmail.com>
2022-09-25 14:31:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ process.on("unhandledRejection", (err) => {
 
 app.get("/", async function (req, res) {
   res.send("It's work.");
-  //await apiChatApi("557999889371", "TESTEEEEE");
 });
 
 const chats_ativos = [];
@@ -57,9 +56,9 @@ app.post("/webhook", function (req, res) {
     }
   }
   else if(session.sequencia == 2){
-    resposta_to = `Olá, ${message_user}!\nAgora me informe seu CPF.`;
     chats_ativos[i].nome = message_user.toUpperCase();
     chats_ativos[i].sequencia = 3;
+    resposta_to = `Olá, ${message_user}!\nAgora me informe seu CPF.`;
   }
   else if(session.sequencia == 3){
     chats_ativos[i].cpf = message_user;
@@ -70,59 +69,10 @@ app.post("/webhook", function (req, res) {
   apiChatApi(phone_number, resposta_to);
 
   res.send({phone_number, resposta_to});
-  /*   
-    for (let i in data.messages) {
-        const author = data.messages[i].author;
-        const body = data.messages[i].body;
-        const chatId = data.messages[i].chatId;
-        const senderName = data.messages[i].senderName;
-        if (data.messages[i].fromMe) continue;
-
-        if (/help/.test(body)) {
-            const text = `${senderName},  this is a demo bot for https://chat-api.com/.
-            Commands:
-            1. chatId - view the current chat ID
-            2. file [pdf/jpg/doc/mp3] - get a file
-            3. ptt - get a voice message
-            4. geo - get a location
-            5. group - create a group with you and the bot`;
-            await apiChatApi('message', {chatId: chatId, body: text});
-        } else if (/chatId/.test(body)) {
-            await apiChatApi('message', {chatId: chatId, body: chatId});
-        } else if (/file (pdf|jpg|doc|mp3)/.test(body)) {
-            const fileType = body.match(/file (pdf|jpg|doc|mp3)/)[1];
-            const files = {
-                doc: botUrl + "/tra.docx",
-                jpg: botUrl + "/tra.jpg",
-                mp3: botUrl + "/tra.mp3",
-                pdf: botUrl + "/tra.pdf"
-            };
-            let dataFile = {
-                phone: author,
-                body: files[fileType],
-                filename: `File *.${fileType}`
-            };
-            if (fileType === "jpg") dataFile['caption'] = "Text under the photo.";
-            await apiChatApi('sendFile', dataFile);
-        } else if (/ptt/.test(body)) {
-            await apiChatApi('sendAudio', {audio: botUrl + "/tra.ogg", chatId: chatId});
-        } else if (/geo/.test(body)) {
-            await apiChatApi('sendLocation', {lat: 51.178843, lng: -1.826210, address: 'Stonehenge', chatId: chatId});
-        } else if (/group/.test(body)) {
-            let arrayPhones = [author.replace("@c.us", "")];
-            await apiChatApi('group', {
-                groupName: 'Bot group',
-                phones: arrayPhones,
-                messageText: 'Welcome to the new group!'
-            });
-        }
-    }
-    res.send('Ok'); */
 });
 
 app.get("/webhook", async function (req, res) {
   const data = req.query;
-  /* console.log(data); */
   res.send(data["hub.challenge"]);
 });
 
@@ -131,7 +81,6 @@ app.listen(process.env.PORT ?? 3000, function () {
 });
 
 async function apiChatApi(telefone, message) {
-
     let phone = telefone;
     if (phone.length != 13) {
         const a = phone.split("");
@@ -157,10 +106,5 @@ async function apiChatApi(telefone, message) {
       Authorization:
         "Bearer EAAFuiawAhM4BAKjJo0Vb6eH8UR9cNymqT0p2cmH2saZCG7J5J6xtZBQtvHOqwFoD3GHtQNxZAdAkeCcSybzZB7gT95jRVbP6iJgawuaYyxyxsZCZBjboqefClAE0gxpUQSCubxVxNDiiG5hcTFD9QjyFM03UMRg5udl3p00d0QZBADxh08uN58mBhyJTsfuhYtG02d5WZCDZBQS1uHyZCRDuobhkoAJG0nxZCYZD",
     },
-  });
-
-
-
-
-  
+  });  
 }
