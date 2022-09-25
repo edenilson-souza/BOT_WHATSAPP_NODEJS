ab6763d112477348473f54a6ed90edff26fb134e
Edenilson Souza <edenilson.sza@gmail.com>
2022-09-24 22:58:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.post("/webhook", async function (req, res) {
 
   if (phone_number.length != 13) {
     const a = phone_number.split("");
-    phone_number = a[0]+a[1]+a[2]+a[3]+'9'+a[4]+a[5]+a[6]+a[7]+a[8]+a[9]+a[10]+a[11]+a[12];
+    phone_number = a[0]+a[1]+a[2]+a[3]+'9'+a[4]+a[5]+a[6]+a[7]+a[8]+a[9]+a[10]+a[11];
   }
 
   const messageeee = "Oi eu sou um CHAT BOT, em que posso te ajudar?";
